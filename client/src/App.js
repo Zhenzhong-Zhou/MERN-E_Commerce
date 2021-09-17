e02426e365148e2167f73cc0e809e64dc77515d3
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-16 21:41:05 -0600

    Initial home page into client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Home} from "./pages";
+
 const App = () => {
 	return (
-		<div>
-			App
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path={"/"} component={Home}/>
+			</Switch>
+		</BrowserRouter>
 	);
 };
 
