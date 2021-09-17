5a92e0817714849e447fa070689ef49cc5e16cca
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-17 00:09:38 -0600

    Initial categories component

---
+++ client/src/components/Categories/Categories.jsx
@@ -0,0 +1,12 @@
+import {Container} from "../../styles/categories";
+import {categories} from "../../boilerplateData";
+
+const Categories = () => {
+	return (
+		<Container>
+			Categories
+		</Container>
+	);
+};
+
+export default Categories;
---
+++ client/src/components/Categories/Category/Category.jsx
@@ -0,0 +1,11 @@
+import {Container} from "../../../styles/category";
+
+const Category = () => {
+	return (
+		<Container>
+			Category
+		</Container>
+	);
+};
+
+export default Category;
