9a715b5ec391aaef1809a4d3a62209a2fac1371d
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-16 22:54:17 -0600

    Add navbar component into home page, and add new dependencies

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
+import {Search, ShoppingCartOutlined} from "@material-ui/icons";
+import {Badge} from "@material-ui/core";
+import {Center, Container, Input, Language, Left, Logo, MenuItem, Right, SearchContainer, Wrapper} from "../../styles/navbar.js";
+
 const Navbar = () => {
 	return (
-		<div>
-			Navbar
-		</div>
+		<Container>
+			<Wrapper>
+				<Left>
+					<Language>
+						EN
+					</Language>
+					<SearchContainer>
+						<Input/>
+						<Search style={{color: "gray", fontSize: "16px"}}/>
+					</SearchContainer>
+				</Left>
+				<Center><Logo>E-Commerce</Logo></Center>
+				<Right>
+					<MenuItem>REGISTER</MenuItem>
+					<MenuItem>SIGN IN</MenuItem>
+					<MenuItem>
+						<Badge badgeContent={4} color={"primary"}>
+							<ShoppingCartOutlined/>
+						</Badge>
+					</MenuItem>
+				</Right>
+			</Wrapper>
+		</Container>
 	);
 };
 
