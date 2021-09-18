14e3872a9bdec90b7a3a48efe970433ec9e91b73
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-18 00:07:58 -0600

    Add register page into client app

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,24 @@
-import {Container} from "../../styles/register";
+import {Agreement, Button, Container, Form, Input, Title, Wrapper} from "../../styles/register";
 
 const Register = () => {
 	return (
 		<Container>
-			Register
+			<Wrapper>
+				<Title>CREATE AN ACCOUNT</Title>
+				<Form>
+					<Input placeholder={"Username"}/>
+					<Input placeholder={"First Name"}/>
+					<Input placeholder={"Last Name"}/>
+					<Input placeholder={"Email"}/>
+					<Input placeholder={"Password"}/>
+					<Input placeholder={"Confirmed Password"}/>
+					<Agreement>
+						By creating an account, I consent to the processing of my personal
+						data in accordance with the <b>PRIVACY POLICY</b>
+					</Agreement>
+					<Button>CREATE</Button>
+				</Form>
+			</Wrapper>
 		</Container>
 	);
 };
--- client/src/styles/register.js
+++ client/src/styles/register.js
@@ -1,3 +1,54 @@
 import styled from "styled-components";
+import registerBg from "../assets/images/register_bg.jpeg";
 
-export const Container = styled.div ``;
+export const Container = styled.div `
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), 
+  				url(${registerBg}) center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div `
+  width: 40%;
+  height: 30%;
+  padding: 20px;
+  background-color: white;
+`;
+
+export const Title = styled.h1 `
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+export const Form = styled.form `
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+`;
+
+export const Input = styled.input `
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`;
+
+export const Agreement = styled.span `
+  font-size: 12px;
+  margin: 20px 0;
+`;
+
+export const Button = styled.button `
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  bottom: -40px;
+`;
