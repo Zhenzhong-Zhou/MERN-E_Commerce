9c1dfa5854500604b6498aea0bfd1a08f819df71
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-19 02:58:59 -0600

    Add login route into auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import CryptoJS from "crypto-js";
 import User from "../models/user.js";
 
 // REGISTER
@@ -5,11 +7,25 @@ export const register = async (req, res) => {
 	const newUser =  new User({
 		username: req.body.username,
 		email: req.body.email,
-		password: req.body.password
+		password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY)
 	});
 	try {
 		const user = await newUser.save();
 		res.status(201).json(user);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// LOGIN
+export const login = async (req, res) => {
+	try {
+		const user = await User.findOne({username: req.body.username});
+		!user && res.status(401).json("Incorrect email or password. Please try again.");
+		const decrypted = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+		const original = decrypted.toString(CryptoJS.enc.Utf8);
+		original !== req.body.password && res.status(401).json("Incorrect email or password. Please try again.");
+		const {password, ...info} = user._doc;
+		res.status(200).json(info);
 	} catch (error) {
 		res.status(500).json(error);
 	}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {register} from "../controllers/auth.js";
+import {login, register} from "../controllers/auth.js";
 
 const router = express.Router();
 
 // REGISTER
 router.post("/register", register);
+// LOGIN
+router.post("/login", login);
 
 export default router;
