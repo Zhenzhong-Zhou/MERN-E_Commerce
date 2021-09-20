606cf4564196d57b1f7dd6c48dfe59a56c2b3646
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-20 01:01:47 -0600

    Type errors

--- server/models/cart.js
+++ server/models/cart.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema({
-		userId: {type: String, required: true},
-		products: [
-			{
-				productId: {type: String},
-				quantity: {type: Number, default: 1}
-			}
-		],
+	userId: {type: String, required: true},
+	products: [
+		{
+			productId: {type: String},
+			quantity: {type: Number, default: 1}
+		}
+	],
 	},
 	{timestamps: true}
 );
--- server/models/order.js
+++ server/models/order.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-		userId: {type: String, required: true},
-		products: [
-			{
-				productId: {type: String},
-				quantity: {type: Number, default: 1}
-			}
-		],
+	userId: {type: String, required: true},
+	products: [
+		{
+			productId: {type: String},
+			quantity: {type: Number, default: 1}
+		}
+	],
 	amount: {type: Number, required: true},
 	address: {type: Object, required: true},
 	status: {type: String, default: "pending"}
