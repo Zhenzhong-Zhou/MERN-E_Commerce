c9c67d436a8106a204f464139fb8814e375c5009
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-19 23:23:02 -0600

    Add fetch user status route and controller

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {verifyTokenAndAdmin, verifyTokenAndAuthorization} from "../middlewares/verifyToken.js";
-import {fetch_all_users, fetch_user, remove, update} from "../controllers/users.js";
+import {fetch_all_users, fetch_user, fetch_user_status, remove, update} from "../controllers/users.js";
 
 const router = express.Router();
 
@@ -12,5 +12,7 @@ router.delete("/:id", verifyTokenAndAuthorization, remove);
 router.get("/find/:id", verifyTokenAndAdmin, fetch_user);
 // FETCH ALL USERS
 router.get("/", verifyTokenAndAdmin, fetch_all_users);
+// FETCH USER STATUS
+router.get("/status", verifyTokenAndAdmin, fetch_user_status);
 
 export default router;
