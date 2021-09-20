d28eee591495b2c663b08bba729d161aff8d35a2
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-20 00:08:58 -0600

    Add products route and controller

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -19,3 +19,39 @@ export const update = async (req, res) => {
 		res.status(500).json(error);
 	}
 };
+// DELETE
+export const remove = async (req, res) => {
+	try {
+		await Product.findByIdAndDelete(req.params.id);
+		res.status(200).json("Product has been DELETED!");
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// FETCH PRODUCT
+export const fetch_product = async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.status(200).json(product);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// FETCH ALL PRODUCTS
+export const fetch_all_products = async (req, res) => {
+	const queryNew = req.query.new;
+	const queryCategory = req.query.categories;
+	try {
+		let products;
+		if (queryNew) {
+			products = await Product.find().sort({createdAt: -1}).limit(1);
+		} else if (queryCategory) {
+			products = await Product.find({categories: {$in: [queryCategory]}});
+		} else {
+			products = await Product.find();
+		}
+		res.status(200).json(products);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {verifyTokenAndAdmin, verifyTokenAndAuthorization} from "../middlewares/verifyToken.js";
-import {create, update} from "../controllers/products.js";
+import {create, fetch_all_products, fetch_product, remove, update} from "../controllers/products.js";
 
 const router = express.Router();
 
@@ -8,5 +8,11 @@ const router = express.Router();
 router.post("/", verifyTokenAndAdmin, create);
 // UPDATE
 router.put("/:id", verifyTokenAndAdmin, update);
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, remove);
+// FETCH PRODUCT
+router.get("/find/:id", fetch_product);
+// FETCH ALL PRODUCTS
+router.get("/", fetch_all_products);
 
 export default router;
