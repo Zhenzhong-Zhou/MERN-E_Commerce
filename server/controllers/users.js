a67375ece0d954106c730a4f225e699c64a1f356
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-19 22:53:09 -0600

    Add user update and delete routes and controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,24 @@
-export const index = (req, res) => {
-	res.send("Welcome to E-Commerce App Backend!")
+import CryptoJS from "crypto-js";
+import User from "../models/user.js";
+
+// UPDATE
+export const update = async (req, res) => {
+	if (req.body.password) {
+		req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY);
+	}
+	try {
+		const updateUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+		res.status(200).json(updateUser);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// DELETE
+export const remove = async (req, res) => {
+	try {
+		await User.findByIdAndDelete(req.params.id);
+		res.status(200).json("User has been DELETED!");
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,12 @@
 import express from "express";
+import {verifyTokenAndAuthorization} from "../middlewares/verifyToken.js";
+import {remove, update} from "../controllers/users.js";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, update);
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, remove);
 
 export default router;
