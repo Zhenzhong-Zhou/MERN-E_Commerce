7d2668e26ce3e0a2a31d3b49522b2b64fdc80088
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-19 22:48:57 -0600

    Update verify token

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -16,10 +16,20 @@ export const verifyToken = (req, res ,next) => {
 
 export const verifyTokenAndAuthorization = (req, res, next) => {
 	verifyToken(req, res, () => {
-		if (req.user.id === req.params.id || req.user.isTester || req.user.isAdmin) {
+		if (req.user.id === req.params.id || req.user.isAdmin) {
 			next();
 		} else {
 			res.status(403).json("Invalid Action!");
+		}
+	});
+}
+
+export const verifyTokenAndAdmin = (req, res, next) => {
+	verifyToken(req, res, () => {
+		if (req.user.isAdmin) {
+			next();
+		} else {
+			res.status(403).json("Invalid Account!");
 		}
 	});
 }
