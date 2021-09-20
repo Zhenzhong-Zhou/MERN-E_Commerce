9c6b2246af6bdb3c8b85fff670a69a41dd2d31a9
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-19 22:14:46 -0600

    Add verify token

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,5 +1,15 @@
-import express from "express";
+import jwt from "jsonwebtoken";
+import {token} from "morgan";
 
-const router = express.Router();
-
-export default router;
+const verifyToken = (req, res ,next) => {
+	const authHeader = req.headers.token;
+	if (authHeader) {
+		jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN, (error, user) => {
+			if (error) res.status(403).json("Invalid Token!");
+			req.user = user;
+			next();
+		});
+	} else {
+		return res.status(401).json("Invalid Authenticated!");
+	}
+};
