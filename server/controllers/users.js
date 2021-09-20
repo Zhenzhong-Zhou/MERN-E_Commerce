423906dbf9723b028938692487205c928a4ed6c6
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-19 23:09:49 -0600

    Add fetch user and fetch all users routes and controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -22,3 +22,23 @@ export const remove = async (req, res) => {
 		res.status(500).json(error);
 	}
 };
+// FETCH USER
+export const fetch_user = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const {password, ...info} = user._doc;
+		res.status(200).json(info);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// FETCH ALL USERS
+export const fetch_all_users = async (req, res) => {
+	const query = req.query.new;
+	try {
+		const users = query ? await User.find().sort({_id: -1}).limit(5) : await User.find();
+		res.status(200).json(users);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {verifyTokenAndAuthorization} from "../middlewares/verifyToken.js";
-import {remove, update} from "../controllers/users.js";
+import {verifyTokenAndAdmin, verifyTokenAndAuthorization} from "../middlewares/verifyToken.js";
+import {fetch_all_users, fetch_user, remove, update} from "../controllers/users.js";
 
 const router = express.Router();
 
@@ -8,5 +8,9 @@ const router = express.Router();
 router.put("/:id", verifyTokenAndAuthorization, update);
 // DELETE
 router.delete("/:id", verifyTokenAndAuthorization, remove);
+// FETCH USER
+router.get("/find/:id", verifyTokenAndAdmin, fetch_user);
+// FETCH ALL USERS
+router.get("/", verifyTokenAndAdmin, fetch_all_users);
 
 export default router;
