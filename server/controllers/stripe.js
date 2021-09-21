ca639532002a3b1b4de73408c7d34e6374a938ba
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-20 21:32:20 -0600

    Add payment route and controller

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,3 +1,18 @@
-export const payment = async (req, res) => {
+import Stripe from 'stripe';
 
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// PAYMENT
+export const payment = (req, res) => {
+	stripe.charges.create({
+		source: req.body.tokenId,
+		amount: req.body.amount,
+		currency: "cad"
+	}, (stripeErr, stripeRes) => {
+		if (stripeErr) {
+			res.status(500).json(stripeErr);
+		} else {
+			res.status(200).json(stripeRes);
+		}
+	}).then(r => console.log(r));
 };
--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,9 +1,7 @@
 import express from "express";
-import Stripe from 'stripe';
 import {payment} from "../controllers/stripe.js";
 
 const router = express.Router();
-const stripe = new Stripe(process.env.STRIPE_KEY);
 
 // PAYMENT
 router.post("/payment", payment);
