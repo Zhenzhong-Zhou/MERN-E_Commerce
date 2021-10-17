99303fa67eae7b0e58bfad88d62be2e27266981f
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-17 13:57:10 -0600

    Modified product model

--- server/models/product.js
+++ server/models/product.js
@@ -5,9 +5,10 @@ const productSchema = new mongoose.Schema({
 	description: {type: String, required: true, min: 50},
 	image: {type: String, required: true},
 	categories: {type: Array},
-	size: {type: String},
-	color: {type: String},
-	price: {type: Number},
+	size: {type: Array},
+	color: {type: Array},
+	price: {type: Number, required: true},
+	inStock: {type: Boolean, default: true}
 	},
 	{timestamps: true}
 );
