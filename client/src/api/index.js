7d2c7e05e0db8d58a2a36dcc2ad39a64779cfc10
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-18 00:24:29 -0600

    Prepare to deploy and fix bugs

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_LOCAL_URL
+	baseURL: process.env.REACT_APP_REMOTE_URL
 });
 
 export const axiosUser = axios.create({
