a71e0c94f9f4d6ab8b964051369285dd3af11d66
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-17 18:12:52 -0600

    Fetch data from database and add some useEffect

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,12 +1,49 @@
+import {useEffect, useState} from "react";
 import {Container} from "../../styles/products";
 import Product from "./Product/Product";
-import {popularProducts} from "../../boilerplateData";
+import {axiosInstance} from "../../api";
 
 const Products = ({category, filters, sort}) => {
+	const [products, setProducts] = useState([]);
+	const [filteredProducts, setFilteredProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const {data} = await axiosInstance.get(category ? `products?categories=${category}` : "products");
+				setProducts(data);
+				console.log(data)
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchProducts();
+	}, [category]);
+
+	useEffect(() => {
+		category && setFilteredProducts(
+			products.filter((item) =>
+				Object.entries(filters).every(([key, value]) =>
+					item[key].includes(value)
+				)
+			)
+		);
+	}, [products, category, filters]);
+
+	useEffect(() => {
+		if (sort === "newest") {
+			setFilteredProducts(prevState => [...prevState.sort((a, b) => a.createdAt - b.createdAt)]);
+		} else if (sort === "asc") {
+			setFilteredProducts(prevState => [...prevState.sort((a, b) => a.price - b.price)]);
+		} else {
+			setFilteredProducts(prevState => [...prevState.sort((a, b) => b.price - a.price)]);
+		}
+	}, [sort]);
+
 	return (
 		<Container>
-			{popularProducts.map((item) => (
-				<Product item={item} key={item.id}/>
+			{filteredProducts.map((item) => (
+				<Product item={item} key={item._id}/>
 			))}
 		</Container>
 	);
