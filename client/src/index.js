a6219a98680f1ae4825bbccb610f5297565be450
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-19 16:08:31 -0600

    Fix bugs

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import reactDOM from "react-dom";
-import App from "./App";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
+import App from "./App";
 import {store, persi_store} from "./redux/store";
 
 reactDOM.render(
 	<Provider store={store}>
-		<PersistGate loading={null} persistor={persi_store}>
+		<PersistGate loading={"null"} persistor={persi_store}>
 			<App />
 		</PersistGate>
 	</Provider>, document.getElementById("root"));
