f976121aa4264e395f46ff0675cfce1bdb23df19
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-19 16:40:01 -0600

    Add css into login page

--- admin/src/pages/Login/Login.jsx
+++ admin/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {useDispatch} from "react-redux";
 import {login} from "../../redux/api";
+import "./styles.css";
 
 const Login = () => {
 	const [username, setUsername] = useState("");
@@ -13,7 +14,8 @@ const Login = () => {
 	};
 
 	return (
-		<div>
+		<div className={"login"}>
+			<h1>E_Commerce Dashboard</h1>
 			<input type={"text"} placeholder={"Username"} onChange={event => setUsername(event.target.value)}/>
 			<input type={"password"} placeholder={"Password"} onChange={event => setPassword(event.target.value)}/>
 			<button onClick={handleClick}>Login</button>
