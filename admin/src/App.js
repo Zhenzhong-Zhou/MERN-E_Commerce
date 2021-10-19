b1b3fbbc9560349ef6db543a4f09b5c67ab1d270
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-19 15:52:49 -0600

    Shorten route code

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,36 +1,23 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {NavBar, SideBar} from "./components";
 import "./App.css";
-import {Home, NewProduct, NewUser, Product, ProductList, User, UserList} from "./pages";
+import {Home, Login, NewProduct, NewUser, Product, ProductList, User, UserList} from "./pages";
 
 const App = () => {
 	return (
 		<BrowserRouter>
+			<Route exact path={"/login"} component={() => <Login/>}/>
 			<NavBar/>
 			<div className={"container"}>
 				<SideBar/>
 				<Switch>
-					<Route exact path={"/"}>
-						<Home/>
-					</Route>
-					<Route exact path={"/users"}>
-						<UserList/>
-					</Route>
-					<Route exact path={"/user/:userId"}>
-						<User/>
-					</Route>
-					<Route exact path={"/newUser"}>
-						<NewUser/>
-					</Route>
-					<Route exact path={"/products"}>
-						<ProductList/>
-					</Route>
-					<Route exact path={"/product/:productId"}>
-						<Product/>
-					</Route>
-					<Route exact path={"/newProduct"}>
-						<NewProduct/>
-					</Route>
+					<Route exact path={"/"} component={Home}/>
+					<Route exact path={"/users"} component={UserList}/>
+					<Route exact path={"/user/:userId"} component={User}/>
+					<Route exact path={"/newUser"} component={NewUser}/>
+					<Route exact path={"/products"} component={ProductList}/>
+					<Route exact path={"/product/:productId"} component={Product}/>
+					<Route exact path={"/newProduct"} component={NewProduct}/>
 				</Switch>
 			</div>
 		</BrowserRouter>
