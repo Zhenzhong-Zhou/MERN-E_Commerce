9a39571a78a49b30707f2710385bdff197f3aed6
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-19 19:46:40 -0600

    Fetch chart data

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -1,12 +1,39 @@
+import {useEffect, useMemo, useState} from "react";
 import "./styles.css";
 import {Chart, FeaturedInfo, WidgetLg, WidgetSm} from "../../components";
-import {userData} from "../../boilerplateData";
+import {axiosUser} from "../../api";
 
 const Home = () => {
+	const [userStatus, setUserStatus] = useState([]);
+	const months = useMemo(() => [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December"
+	], []);
+
+	useEffect(() => {
+		const fetchStatus = async () => {
+			try {
+				const {data} = await axiosUser.get("users/status");
+				const statusList = data.sort((a, b) => {return a._id - b._id});
+				statusList.map(item =>
+					setUserStatus(prevState => [
+						...prevState,
+						{month: months[item._id - 1], "Active User": item.total}
+					])
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchStatus();
+	}, [months]);
+
 	return (
 		<div className={"home"}>
 			<FeaturedInfo/>
-			<Chart data={userData} title={"User Analytics"} grid dataKey={"Active User"}/>
+			<Chart data={userStatus} title={"User Analytics"} grid dataKey={"Active User"}/>
 			<div className={"homeWidgets"}>
 				<WidgetSm/>
 				<WidgetLg/>
