5716dc8bd17b2d6f12678b3814feb1928f0a88ac
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-19 20:24:04 -0600

    Fetch revenue data

--- admin/src/components/FeaturdInfo/FeaturedInfo.jsx
+++ admin/src/components/FeaturdInfo/FeaturedInfo.jsx
@@ -1,10 +1,28 @@
+import {useEffect, useState} from "react";
 import "./styles.css";
 import {Cost, Revenue, Sale} from "../index";
+import {axiosUser} from "../../api";
 
 const FeaturedInfo = () => {
+	const [income, setIncome] = useState([]);
+	const [percentage, setPercentage] = useState(0);
+
+	useEffect(() => {
+		const fetchIncome = async () => {
+			try {
+				const {data} = await axiosUser.get("orders/income");
+				setIncome(data);
+				setPercentage((data[1].total*100) / data[0].total - 100)
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchIncome();
+	}, []);
+
 	return (
 		<div className={"featured"}>
-			<Revenue/>
+			<Revenue income={income} percentage={percentage}/>
 			<Sale/>
 			<Cost/>
 		</div>
--- admin/src/components/FeaturdInfo/FeaturedItem/Revenue.jsx
+++ admin/src/components/FeaturdInfo/FeaturedItem/Revenue.jsx
@@ -1,13 +1,20 @@
-import {ArrowDownward} from "@material-ui/icons";
+import {ArrowDownward, ArrowUpward} from "@material-ui/icons";
 import "./styles.css";
 
-const Revenue = () => {
+const Revenue = ({income, percentage}) => {
+	console.log("rev: ", income)
 	return (
 		<div className={"featuredItem"}>
 			<span className={"featuredTitle"}>Revenue</span>
 			<div className={"featuredMoneyContainer"}>
-				<span className={"featuredMoney"}>$2,415</span>
-				<span className={"featuredMoneyRate"}>-11.4<ArrowDownward className={"featuredIcon negative"}/></span>
+				<span className={"featuredMoney"}>${income.map(i => i.total)[0]}</span>
+				<span className={"featuredMoneyRate"}>{Math.floor(percentage)}%
+					{percentage < 0 ? (
+						<ArrowDownward className={"featuredIcon negative"}/>
+					) : (
+						<ArrowUpward className={"featuredIcon"}/>
+					)}
+					</span>
 			</div>
 			<span className={"featuredSub"}>Compared to last month</span>
 		</div>
